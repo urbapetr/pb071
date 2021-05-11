45573e003bf288a2f65243b2dfe7d8c3f05f1dfd
Petr Urbánek <xurbane3@fi.muni.cz>
2021-05-11 18:14:48 +0200

    hw04

--- hw04/src/ini_parser.c
+++ hw04/src/ini_parser.c
@@ -1,53 +1,12 @@
 #include <stdio.h>
 #include "ini_parser.h"
 
-#include "view.h"
-
-static void help_command(const char *program)
-{
-    const char *help =
-            "Usage: %s OPTIONS\n"
-            "\n"
-            "This program loads a sudoku and takes following options:\n"
-            "\n"
-            "\t-h, --help\t\t Show OPTIONS of this program (nothing else)\n"
-            "\n"
-            "\t-d N, --max-depth N\tMaximum depth, where N must be number\n"
-            "\t\t\t\t-> 0 = not allowed\n"
-            "\t\t\t\t-> minus value -> unlimited\n"
-            "\t\t\t\t-> if N is not declared, then N = 10\n"
-            "\n"
-            "\t-g, --include-guard\tEvery file can be included only for first used time\n"
-            "\n"
-            "\t-r, --report-cycles\tDetecting cycling program\n"
-            "\n";
-
-    printf(help, program);
-}
-
-static void depth_command(const char *program, int N)
-{
-    printf("\n in DEPTH \n");
-}
-
-static void guard_command(const char *program)
-{
-    printf("\n in GUARD \n");
-}
-
-static void cycles_command(const char *program)
-{
-    printf("\n in CYCLE \n");
-}
-
-int readline(FILE *base_file, FILE *result_file, bool cycle, bool guard, int depth, char *first_file);
 
 int main(int argc, char **argv)
 {
     int d = 10;
     bool guard = false;
     bool cycle = false;
-    bool have_files = false;
     FILE *base_file;
     FILE *result_file;
     int i = 1;
@@ -66,7 +25,6 @@ int main(int argc, char **argv)
                 i++;
                 continue;
             }
-            depth_command(argv[0], 10);
             i++;
         }
         else if ((strcmp(argv[i], "-g") == 0) || (strcmp(argv[i], "--include-guard") == 0))
@@ -100,36 +58,5 @@ int main(int argc, char **argv)
 
     fclose(base_file);
     fclose(result_file);
-    return EXIT_SUCCESS;
-}
-
-
-int readline(FILE *base_file, FILE *result_file, bool cycle, bool guard, int depth, char *first_file) {
-    if (depth == 0) {
-        return EXIT_SUCCESS;
-    }
-    char all_lines[255];
-    while (fgets(all_lines, 255, base_file)) {
-        if ((strstr(".include", all_lines)) != NULL) {
-            bool skip = false;
-            if (cycle) {
-                if (strcmp(&all_lines[9], first_file) == 0) {
-                        return EXIT_FAILURE;
-                }
-            }
-            if (guard) {
-                if (strcmp(&all_lines[9], first_file) == 0) {
-                    skip = true;
-                    break;
-                }
-            }
-            FILE *next_file;
-            next_file = fopen(&all_lines[9], "r");
-            readline(next_file, result_file, cycle, guard, depth-1, (char *) &first_file);
-            fclose(next_file);
-            continue;
-        }
-        fputs(all_lines, result_file);
-    }
     return EXIT_SUCCESS;
 }
