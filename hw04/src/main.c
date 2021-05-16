f8c5275d500064d4e99171a0f6067f6d341bf482
Petr Urbánek <xurbane3@fi.muni.cz>
2021-05-16 15:12:19 +0200

    hw04

--- hw04/src/main.c
+++ hw04/src/main.c
@@ -4,6 +4,26 @@
 
 int main(int argc, char **argv)
 {
+    const char *help =
+            "Help table for this program:\n"
+            "\n"
+            "This program loads .ini files into one file and takes following options:\n"
+            "\n"
+            "\t-h, --help\t\t Show OPTIONS of this program (nothing else)\n"
+            "\n"
+            "\t-d N, --max-depth N\tMaximum depth, where N must be number\n"
+            "\t\t\t\t-> 0 = not allowed\n"
+            "\t\t\t\t-> minus value -> unlimited\n"
+            "\t\t\t\t-> if N is not declared, then N = 10\n"
+            "\n"
+            "\t-g, --include-guard\tEvery file can be included only for first used time\n"
+            "\n"
+            "\t-r, --report-cycles\tDetecting cycling program\n"
+            "\n";
+    if (argc <= 3) {
+        fprintf(stderr,"%s", help);
+        return EXIT_FAILURE;
+    }
     int d = 10;
     bool guard = false;
     bool cycle = false;
@@ -14,7 +34,7 @@ int main(int argc, char **argv)
     {
         if ((strcmp(argv[i], "-h") == 0) || (strcmp(argv[i], "--help") == 0))
         {
-            help_command(argv[0]);
+            fprintf(stdout,"%s", help);
             return EXIT_SUCCESS;
         }
         else if ((strcmp(argv[i], "-d") == 0) || (strcmp(argv[i], "--max-depth") == 0))
@@ -38,13 +58,14 @@ int main(int argc, char **argv)
             i++;
         }
         else {
-            help_command(argv[0]);
+            fprintf(stderr,"%s", help);
             return EXIT_FAILURE;
         }
     }
 
-    if (((strstr(".ini", argv[argc-2])) == NULL) &&
+    if (((strstr(".ini", argv[argc-2])) == NULL) ||
         (strstr(".ini", argv[argc-1])) == NULL) {
+        fprintf(stderr, "Not two .ini files");
         return EXIT_FAILURE;
     }
     base_file = fopen(argv[argc-2], "r");
@@ -53,6 +74,7 @@ int main(int argc, char **argv)
     if (readline(base_file, result_file, cycle, guard, d, argv[argc-2]) == EXIT_FAILURE) {
         fclose(base_file);
         fclose(result_file);
+        fprintf(stderr, "Something bad in files");
         return EXIT_FAILURE;
     }
 
